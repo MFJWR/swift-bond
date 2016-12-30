db1c6ea3ab403d274becc3a0ce64ff5b5f4ce7a4
kentrino <kentrino@h-kento.jp>
2016-12-30 15:24:59 +0900

    this is minimum example.

--- SwiftBond/StartViewController.swift
+++ SwiftBond/StartViewController.swift
@@ -1,23 +1,15 @@
-//
-//  ViewController.swift
-//  SwiftBond
-//
-//  Created by Kento Haneda on 2016/12/18.
-//  Copyright © 2016年 Kento Haneda. All rights reserved.
-//
-
 import UIKit
+import Bond
 
 class StartViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        input.bnd_text
+            .bind(to: label.bnd_text)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
